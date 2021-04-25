653c78a921973c32d9416428aeada9dd1f59be5e
TaoTao <hantaotaohan@gmail.com>
2021-04-25 12:32:59 +0800

    Update install_tools.sh

--- .extras/install/install_tools.sh
+++ .extras/install/install_tools.sh
@@ -85,7 +85,7 @@ Alttab() {
     sudo apt install -y -qq libx11-dev libxmu-dev libxft-dev libxrender-dev libxrandr-dev libpng-dev uthash-dev ibxpm-dev libxpm4 > /dev/null 2>&1 
     git clone -q https://hub.fastgit.org/sagb/alttab.git $Bin_dir/alttab && cd $Bin_dir/alttab
     ./configure 
-    make > /dev/null 2>&1 
+    make -s
     sudo make install 
     sudo rm -rf $Bin_dir/alttab
     alttabversion=$(alttab -v 2>&1)
