5c16e3efc259866f18ba23e2a62a0adb86812039
TaoTao <hantaotaohan@gmail.com>
2021-04-26 10:26:39 +0800

    Update install_tools.sh

--- .extras/install/install_tools.sh
+++ .extras/install/install_tools.sh
@@ -195,7 +195,7 @@ Crossover() {
 I3gaps() {
     sudo apt install -y -qq dh-autoreconf libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev xcb libxcb1-dev libxcb-icccm4-dev \
     libyajl-dev libev-dev libxcb-xkb-dev libxcb-cursor-dev libxkbcommon-dev libxcb-xinerama0-dev libxkbcommon-x11-dev \
-    libstartup-notification0-dev libxcb-randr0-dev libxcb-xrm0 libxcb-xrm-dev libxcb-shape0 libxcb-shape0-dev
+    libstartup-notification0-dev libxcb-randr0-dev libxcb-xrm0 libxcb-xrm-dev libxcb-shape0 libxcb-shape0-dev > /dev/null 2>&1
     git clone -q https://hub.fastgit.org/Airblader/i3 $Bin_dir/i3-gaps
     cd $Bin_dir/i3-gaps
     mkdir -p build && cd build
