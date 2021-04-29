4b6e494a70259b0ed1ec3ad292729509919daa31
TaoTao <hantaotaohan@gmail.com>
2021-04-29 11:37:42 +0800

    Update install_tools.sh

--- .extras/install/install_tools.sh
+++ .extras/install/install_tools.sh
@@ -9,7 +9,7 @@ Gitclone=$(ping -c 1 -w 1 github.com)
 Fastclone=$(ping -c 1 -w 1 hub.fastgit.org)
 Fastdownload=$(ping -c 1 -w 1 download.fastgit.org)
 
-if [[ $Fastclone ]] && [[ $Gitclone ]] && [[ $Gitpingspeed -gt 200 ]]; then
+if $Fastclone && $Gitclone && [ $Gitpingspeed -gt 200 ]; then
     Git_clone="https://hub.fastgit.org"
     Git_download="https://download.fastgit.org"
 else
