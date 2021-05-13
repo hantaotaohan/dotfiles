dd522ccddb349f6bd75c614c2f31cc06ff236a1a
TaoTao <hantaotaohan@gmail.com>
2021-05-13 15:48:14 +0800

    Update speedkeyboard.sh

--- .config/i3/speedkeyboard.sh
+++ .config/i3/speedkeyboard.sh
@@ -18,7 +18,7 @@ xset -r 75	# F9
 xset -r 76	# F10
 xset -r 95	# F11
 xset -r 96	# F12
-xset -r 
+
 xset -r 49	# `
 xset -r 10	# 1
 xset -r 11	# 2
@@ -32,30 +32,30 @@ xset -r 18	# 9
 xset -r 19	# 0
 xset -r 20	# -
 xset -r 21	# =
-xset -r 22	# Backspace
-xset -r 107	# Delete
-xset -r 
+# xset -r 22	# Backspace
+# xset -r 107	# Delete
+
 xset -r 23	# Tab
-xset -r 24	# Q
-xset -r 25	# W
-xset -r 26	# E
-xset -r 27	# R
-xset -r 28	# T
-xset -r 29	# Y
-xset -r 30	# U
-xset -r 31	# I
-xset -r 32	# O
-xset -r 33	# P
+# xset -r 24	# Q
+# xset -r 25	# W
+# xset -r 26	# E
+# xset -r 27	# R
+# xset -r 28	# T
+# xset -r 29	# Y
+# xset -r 30	# U
+# xset -r 31	# I
+# xset -r 32	# O
+# xset -r 33	# P
 xset -r 34	# [
 xset -r 35	# ]
 xset -r 51	# \
-xset -r 
+    
 xset -r 66	# Caps Lock
-xset -r 38	# A
-xset -r 39	# S
-xset -r 40	# D
-xset -r 41	# F
-xset -r 42	# G
+# xset -r 38	# A
+# xset -r 39	# S
+# xset -r 40	# D
+# xset -r 41	# F
+# xset -r 42	# G
 # xset -r 43	# H
 # xset -r 44	# J
 # xset -r 45	# K
@@ -63,25 +63,25 @@ xset -r 42	# G
 xset -r 47	# ;
 xset -r 48	# '
 xset -r 36	# Return
-xset -r 
+
 xset -r 50	# Shift Left
-xset -r 52	# Z
-xset -r 53	# X
-xset -r 54	# C
-xset -r 55	# V
-xset -r 56	# B
-xset -r 57	# N
-xset -r 58	# M
+# xset -r 52	# Z
+# xset -r 53	# X
+# xset -r 54	# C
+# xset -r 55	# V
+# xset -r 56	# B
+# xset -r 57	# N
+# xset -r 58	# M
 xset -r 59	# ,
 xset -r 60	# .
 xset -r 61	# /
 xset -r 62	# Shift Right
-xset -r 
-xset -r 100	# Cursor Left
-xset -r 104	# Cursor Down
-xset -r 102	# Cursor Right
-xset -r 98	# Cursor Up
-xset -r 
+
+# xset -r 100	# Cursor Left
+# xset -r 104	# Cursor Down
+# xset -r 102	# Cursor Right
+# xset -r 98	# Cursor Up
+
 xset -r 37	# Ctrl Left
 xset -r 115	# Logo Left (-> Option)
 xset -r 64	# Alt Left (-> Command)
@@ -90,7 +90,7 @@ xset -r 113	# Alt Right (-> Command)
 xset -r 116	# Logo Right (-> Option)
 xset -r 117	# Menu (-> International)
 xset -r 109	# Ctrl Right
-xset -r 
+
 xset -r 111	# PrintScrn
 xset -r 78	# Scroll Lock
 xset -r 110	# Pause
