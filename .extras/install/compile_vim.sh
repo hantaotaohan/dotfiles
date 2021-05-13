a4c1aae41f7173908a83530758ef4a6a68265108
TaoTao <hantaotaohan@gmail.com>
2021-05-13 08:40:10 +0800

    Update compile_vim.sh

--- .extras/install/compile_vim.sh
+++ .extras/install/compile_vim.sh
@@ -18,7 +18,7 @@ sudo ./configure --with-features=huge \
 	--enable-cscope \
 	--enable-gui=auto \
 	# --enable-perlinterp \
-	--with-python-config-dir=$(python2-config --configdir) \
+	#--with-python-config-dir=$(python2-config --configdir) \
 	--with-python3-config-dir=$(python3-config --configdir) \
 	--with-compiledby="TaoTao" \
 	# --enable-largefile \
