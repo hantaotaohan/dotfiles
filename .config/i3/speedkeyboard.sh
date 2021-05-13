2ca9a8094bda94c8bdd9b2eb4183afeae2db1742
TaoTao <hantaotaohan@gmail.com>
2021-05-13 15:43:30 +0800

    Update speedkeyboard.sh

--- .config/i3/speedkeyboard.sh
+++ .config/i3/speedkeyboard.sh
@@ -5,24 +5,99 @@
 xset r rate 180 60
 
 # disable key repeat for keys that are not (or not heavily) used for navigation
-xset -r 23  # tab
-xset -r 49  # grave
-xset -r 10  # key 1
-xset -r 11  # key 2
-xset -r 12  # key 3
-xset -r 13  # key 4
-xset -r 14  # key 5
-xset -r 15  # key 6
-xset -r 16  # key 7
-xset -r 17  # key 8
-xset -r 18  # key 9
-xset -r 19  # key 0
-xset -r 20  # minus
-xset -r 21  # equals
-xset -r 51  # backslash
-xset -r 61  # slash
-xset -r 47  # semicolon ;
-xset -r 48  # apostrophe '
-xset -r 110  # apostrophe HOME
-# xset -r 65  # spacebar
-# xset -r 22  # backspace
+
+xset -r 67	# F1
+xset -r 68	# F2
+xset -r 69	# F3
+xset -r 70	# F4
+xset -r 71	# F5
+xset -r 72	# F6
+xset -r 73	# F7
+xset -r 74	# F8
+xset -r 75	# F9
+xset -r 76	# F10
+xset -r 95	# F11
+xset -r 96	# F12
+xset -r 
+xset -r 49	# `
+xset -r 10	# 1
+xset -r 11	# 2
+xset -r 12	# 3
+xset -r 13	# 4
+xset -r 14	# 5
+xset -r 15	# 6
+xset -r 16	# 7
+xset -r 17	# 8
+xset -r 18	# 9
+xset -r 19	# 0
+xset -r 20	# -
+xset -r 21	# =
+xset -r 22	# Backspace
+xset -r 107	# Delete
+xset -r 
+xset -r 23	# Tab
+xset -r 24	# Q
+xset -r 25	# W
+xset -r 26	# E
+xset -r 27	# R
+xset -r 28	# T
+xset -r 29	# Y
+xset -r 30	# U
+xset -r 31	# I
+xset -r 32	# O
+xset -r 33	# P
+xset -r 34	# [
+xset -r 35	# ]
+xset -r 51	# \
+xset -r 
+xset -r 66	# Caps Lock
+xset -r 38	# A
+xset -r 39	# S
+xset -r 40	# D
+xset -r 41	# F
+xset -r 42	# G
+# xset -r 43	# H
+# xset -r 44	# J
+# xset -r 45	# K
+# xset -r 46	# L
+xset -r 47	# ;
+xset -r 48	# '
+xset -r 36	# Return
+xset -r 
+xset -r 50	# Shift Left
+xset -r 52	# Z
+xset -r 53	# X
+xset -r 54	# C
+xset -r 55	# V
+xset -r 56	# B
+xset -r 57	# N
+xset -r 58	# M
+xset -r 59	# ,
+xset -r 60	# .
+xset -r 61	# /
+xset -r 62	# Shift Right
+xset -r 
+xset -r 100	# Cursor Left
+xset -r 104	# Cursor Down
+xset -r 102	# Cursor Right
+xset -r 98	# Cursor Up
+xset -r 
+xset -r 37	# Ctrl Left
+xset -r 115	# Logo Left (-> Option)
+xset -r 64	# Alt Left (-> Command)
+xset -r 65	# Space
+xset -r 113	# Alt Right (-> Command)
+xset -r 116	# Logo Right (-> Option)
+xset -r 117	# Menu (-> International)
+xset -r 109	# Ctrl Right
+xset -r 
+xset -r 111	# PrintScrn
+xset -r 78	# Scroll Lock
+xset -r 110	# Pause
+xset -r 106	# Insert
+xset -r 97	# Home
+xset -r 99	# Page Up
+xset -r 77	# Num Lock
+xset -r 103	# End
+xset -r 105	# Page Down
+xset -r 94	# International
