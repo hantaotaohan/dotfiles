bb8813f515b44ac1712aaed09904549a599da19f
TaoTao <hantaotaohan@gmail.com>
2021-05-13 13:25:35 +0800

    Update compile_vim.sh

--- .extras/install/compile_vim.sh
+++ .extras/install/compile_vim.sh
@@ -1,25 +1,23 @@
-sudo apt-get remove vim vim-tiny vim-common vim-runtime gvim
-
 git clone https://hub.fastgit.org/vim/vim.git $HOME/.bin/vim
 
 cd $HOME/.bin/vim
 
 sudo apt-get remove --purge vi vim-tiny vim vim-runtime gvim vim-common vim-gui-common vim-nox
 
-sudo apt-get install libncurses5-dev python-dev python3-dev libwxgtk3.0-dev libatk1.0-dev libbonoboui2-dev libcairo2-dev libx11-dev libxpm-dev libxt-dev ncurses*
+sudo apt-get install libncurses5-dev python-dev python3-dev libwxgtk3.0-dev libatk1.0-dev libbonoboui2-dev libcairo2-dev libx11-dev libxpm-dev libxt-dev ncurses* 
 
 sudo ./configure --with-features=huge \
 	--enable-multibyte \
 	--enable-fontset \
-	--enable-rubyinterp \
+	#--enable-rubyinterp \
 	#--enable-pythoninterp \
-	--enable-python3interp \
-	--enable-luainterp \
-	--enable-cscope \
-	--enable-gui=auto \
+	#--enable-python3interp \
+	#--enable-luainterp \
+	#--enable-cscope \
+	#--enable-gui=auto \
 	# --enable-perlinterp \
 	#--with-python-config-dir=$(python2-config --configdir) \
-	--with-python3-config-dir=$(python3-config --configdir) \
+	#--with-python3-config-dir=$(python3-config --configdir) \
 	--with-compiledby="TaoTao" \
 	# --enable-largefile \
 	--prefix=/usr/local/vim
