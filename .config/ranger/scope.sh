a1790fa93ef31b7bfdf09255a9a8b911be7cfab2
TaoTao <hantaotaohan@gmail.com>
2021-09-22 10:41:53 +0800

    Update scope.sh

--- .config/ranger/scope.sh
+++ .config/ranger/scope.sh
@@ -314,7 +314,7 @@ handle_mime() {
             env HIGHLIGHT_OPTIONS="${HIGHLIGHT_OPTIONS}" highlight \
                 --out-format="${highlight_format}" \
                 --force -- "${FILE_PATH}" && exit 5
-            env COLORTERM=8bit bat --color=always --style="plain" --theme="TwoDark" \
+            env COLORTERM=8bit bat --color=always --style="plain" --theme="base16" \
                 -- "${FILE_PATH}" && exit 5
             pygmentize -f "${pygmentize_format}" -O "style=${PYGMENTIZE_STYLE}"\
                 -- "${FILE_PATH}" && exit 5
