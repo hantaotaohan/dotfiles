f29106069bf9b0f58e2f87cdb4ee035e61d517c3
TaoTao <hantaotaohan@gmail.com>
2021-10-13 14:11:14 +0800

    Update weread.js

--- .config/qutebrowser/greasemonkey/weread.js
+++ .config/qutebrowser/greasemonkey/weread.js
@@ -4,19 +4,19 @@
 
 
 /* 字体 */
-GM_addStyle("*{font-family: 'Fira Code','KaiTi'; font-weight:bold ; !important;}");
+GM_addStyle("*{font-family: 'Inziu Iosevka SC','KaiTi'; font-weight:bold ; !important;}");
 
 /* app_content */
 GM_addStyle(".app_content{padding:0,100; !important;}");
 
 /* 顶部工具栏 */
-GM_addStyle(".readerTopBar{max-width:100%; font-family: 'Fira Code','KaiTi' !important;}");
+GM_addStyle(".readerTopBar{max-width:100%; font-family: 'Inziu Iosevka SC','KaiTi' !important;}");
 /* 书标题 */
-GM_addStyle(".readerTopBar_title .readerTopBar_title_link{font-family: 'Fira Code','KaiTi'; !important; font-weight:bold !important;}");
+GM_addStyle(".readerTopBar_title .readerTopBar_title_link{font-family: 'Inziu Iosevka SC','KaiTi'; !important; font-weight:bold !important;}");
 /* 当前章节标题 */
-GM_addStyle(".readerTopBar_title .readerTopBar_title_chapter{font-family: 'Fira Code','KaiTi' !important;}");
+GM_addStyle(".readerTopBar_title .readerTopBar_title_chapter{font-family: 'Inziu Iosevka SC','KaiTi' !important;}");
 /* 去书架查看 */
-GM_addStyle(".readerTopBar_actions .addShelfItem{font-family: 'Fira Code','KaiTi' !important;}");
+GM_addStyle(".readerTopBar_actions .addShelfItem{font-family: 'Inziu Iosevka SC','KaiTi' !important;}");
 
 /* 背影色 */
 GM_addStyle(".wr_whiteTheme .renderTargetContainer .renderTargetContent .wr_readerImage_opacity {background-color: rgba(40,44,52,100) !important;}");
@@ -30,18 +30,18 @@ GM_addStyle(".readerChapterContent{color: rgba(171,178,191,100) !important;}");
 GM_addStyle(".readerChapterContent{font-weight: bold !important;}");
 
 /* 笔记 */
-GM_addStyle(".readerNotePanel .selectionListItem{border:5px solid  #f00; font-family: 'Fira Code','KaiTi';!important;}");
+GM_addStyle(".readerNotePanel .selectionListItem{border:5px solid  #f00; font-family: 'Inziu Iosevka SC','KaiTi';!important;}");
 
 /* 目录 */
-GM_addStyle(".readerCatalog h2.readerCatalog_bookInfo_title .readerCatalog_bookInfo_title_txt{font-family: 'Fira Code','KaiTi' !important;}");
-GM_addStyle(".readerCatalog ul.readerCatalog_list li.chapterItem span.chapterItem_text{font-family: 'Fira Code','KaiTi' !important;}");
-//GM_addStyle(".readerCatalog ul.readerCatalog_list li.chapterItem_current{border:5px solid #ccc; font-family: 'Fira Code','KaiTi' !important;}");
-GM_addStyle(".readerCatalog ul.readerCatalog_list li.chapterItem_current span.chapterItem_text{font-family:  'Fira Code','KaiTi' !important;}");
+GM_addStyle(".readerCatalog h2.readerCatalog_bookInfo_title .readerCatalog_bookInfo_title_txt{font-family: 'Inziu Iosevka SC','KaiTi' !important;}");
+GM_addStyle(".readerCatalog ul.readerCatalog_list li.chapterItem span.chapterItem_text{font-family: 'Inziu Iosevka SC','KaiTi' !important;}");
+//GM_addStyle(".readerCatalog ul.readerCatalog_list li.chapterItem_current{border:5px solid #ccc; font-family: 'Inziu Iosevka SC','KaiTi' !important;}");
+GM_addStyle(".readerCatalog ul.readerCatalog_list li.chapterItem_current span.chapterItem_text{font-family:  'Inziu Iosevka SC','KaiTi' !important;}");
 /* 目录-滚动条（隐藏） */
 GM_addStyle(".readerCatalog_list::-webkit-scrollbar {overflow: hidden ;position: relative ;display: none;};");
 
 /* 下一页 */
-GM_addStyle(".readerFooter .readerFooter_button {font-family: 'Fira Code','KaiTi';font-weight:bold !important;}");
+GM_addStyle(".readerFooter .readerFooter_button {font-family: 'Inziu Iosevka SC','KaiTi';font-weight:bold !important;}");
 
 (function() {
     'use strict';
