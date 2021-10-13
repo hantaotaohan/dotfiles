c1292600cc09cd33e07f85370d99d326e6b910e9
TaoTao <hantaotaohan@gmail.com>
2021-10-13 16:39:57 +0800

    Create github.js

---
+++ .config/qutebrowser/greasemonkey/github.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @match https://github.com/*
+// ==/UserScript==
+
+
+/* 字体 */
+GM_addStyle("*{font-family: 'FiraCode','微软雅黑'; font-size:13px; font-weight:600 ; !important;}");
+
+(function() {
+    'use strict';
+})();
