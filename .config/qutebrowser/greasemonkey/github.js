cf991dd5d168f9a032be2859ac874027ce4db721
TaoTao <hantaotaohan@gmail.com>
2021-10-29 10:34:00 +0800

    Update github.js

--- .config/qutebrowser/greasemonkey/github.js
+++ .config/qutebrowser/greasemonkey/github.js
@@ -4,7 +4,8 @@
 
 
 /* 字体 */
-GM_addStyle("*{font-family: 'FiraCode','微软雅黑'; font-size:13px; font-weight:600 ; !important;}");
+/* GM_addStyle("*{font-family: 'FiraCode','微软雅黑'; font-size:13px; font-weight:600 ; !important;}"); */
+GM_addStyle("*{font-family: BlinkMacSystemFont,'Segoe UI',Helvetica,Arial,sans-serif,'Apple Color Emoji','Segoe UI Emoji'; font-size:14px; line-height:1.5 ;font-weight:600 ; !important;}");
 
 (function() {
     'use strict';
