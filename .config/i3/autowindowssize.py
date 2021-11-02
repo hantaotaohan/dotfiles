d8c871df3f924cdb6db81f3b98d9178755d2b536
TaoTao <hantaotaohan@gmail.com>
2021-11-02 09:57:58 +0800

    Update autowindowssize.py

--- .config/i3/autowindowssize.py
+++ .config/i3/autowindowssize.py
@@ -6,7 +6,7 @@ def on(i3, e):
     i3 = i3ipc.Connection()
     workspace = i3.get_tree().find_focused().workspace().num
     if e.container.window_class in ['Microsoft-edge-dev',  'Alacritty'] and workspace == 10:
-        e.container.command('resize set 0 ppt 90 ppt')
+        e.container.command('resize set 60 ppt 90 ppt')
 
 i3 = i3ipc.Connection()
 i3.on('window::focus', on)
