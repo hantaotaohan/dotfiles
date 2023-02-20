19c2496bd2db45142873799a80621f4b28db38ba
hantaotaohan <hantaotaohan@gmail.com>
2023-02-20 16:19:52 +0800

    fix nvim

--- nvim/lua/plugins/bufferline.lua
+++ nvim/lua/plugins/bufferline.lua
@@ -2,27 +2,24 @@ return {
 
     "akinsho/bufferline.nvim",
     event = "VeryLazy",
-    keys = {
-        { "<Leader>]", "<Cmd>BufferLineTogglePin<CR>", desc = "Toggle pin" },
-    },
     opts = {
         options = {
             mode = "buffers",                                                  -- | "buffers" | "tabs"      |
-            numbers = "ordinal",                                               -- | "none"    | "ordinal"   | "buffer_id" | "both" |
+            numbers = "none",                                                  -- | "none"    | "ordinal"   | "buffer_id" | "both" |
             close_command = "bdelete! %d",                                     -- | string    | function    | see "Mouse actions   |
             right_mouse_command = "bdelete! %d",                               -- | string    | function    | see "Mouse actions   |
             left_mouse_command = "buffer %d",                                  -- | string    | function    | see "Mouse actions   |
             middle_mouse_command = nil,                                        -- | string    | function    | see "Mouse actions   |
             indicator = {
                 icon = '▋',                                                    -- 图标指示器
-                style = 'underline',                                           -- | 'icon'    | 'underline' | 'none' |
+                style = 'icon',                                                -- | 'icon'    | 'underline' | 'none' |
             },
-            buffer_close_icon = '✖' ,
+            buffer_close_icon = '' ,
             modified_icon = '·',
             close_icon = '',
             left_trunc_marker = '',
             right_trunc_marker = '',
-            max_name_length = 18,
+            max_name_length = 15,
             max_prefix_length = 15,                                            -- 缓冲区重复数据消除时使用的前缀
             truncate_names = true,                                             -- 是否应截断选项卡名称
             tab_size = 18,
@@ -35,63 +32,61 @@ return {
                 {
                     filetype = "neo-tree",
                     text = "File Explorer",
-                    text_align = "center",                                     -- | "left" | "center" | "right" |
+                    text_align = "left",                                       -- | "left" | "center" | "right" |
                     separator = true,
                     padding = 0, 
-                    highlight = "BufferLineFill", 
+                    highlight = "BufferLineBufferVisible", 
                 }
             },
             color_icons = true,                                                -- 是否添加文件类型图标突出显示
             show_buffer_icons = true,                                          -- 禁用缓冲区的文件类型图标
-            show_buffer_close_icons = false,
+            show_buffer_close_icons = true,
             show_buffer_default_icon = true,                                   -- 未识别的文件类型是否应显示默认图标
-            show_close_icon = true,
-            show_tab_indicators = true,
-            show_duplicate_prefix = true,                                      -- 是否显示重复的缓冲区前缀
+            show_close_icon = false,
+            show_tab_indicators = false,
+            show_duplicate_prefix = false,                                     -- 是否显示重复的缓冲区前缀
             persist_buffer_sort = true,                                        -- 自定义排序缓冲区是否应持久
-            separator_style = { '', '' },                                      -- | "slant" | "thick" | "thin" | { 'any', 'any' } |
-            enforce_regular_tabs = false ,
+            separator_style = "thin",                                          -- | "slant" | "thick" | "thin" | { 'any', 'any' } |
+            enforce_regular_tabs = true ,
             always_show_bufferline = true,
             hover = {
-                enabled = true,
+                enabled = false,
                 delay = 200,
                 reveal = {'close'}
             },
             sort_by ='insert_after_current',                                   -- | 'insert_after_current' |'insert_at_end' | 'id' | 'extension' | 'relative_directory' | 'directory' | 'tabs' | function(buffer_a, buffer_b)
-            
-            highlights = {
+            name_formatter = function(buf) return "  " .. buf.name end,
+        },
 
-                fill                  = { fg = '#21252b', bg = '#21252b' },
-                -- fill                  = { fg = '#606B70', bg = '#1E2227' },
-                -- background            = { fg = '#606B70', bg = None },
+        highlights = {
 
-                -- background            = {  bg = nil },
-
-                -- close_button          = { fg = '#606B70', bg = '#1E2227' },
-                -- close_button_visible  = { fg = '#606B70', bg = '#1E2227' },
-                -- close_button_selected = { fg = '#DCDCDC', bg = '#23272E' },
+            fill                  = { fg = '#21252B', bg = '#21252B' },
+            -- background            = { fg = '#61afef', bg = '#61afef'},
 
 
+            -- close_button          = { fg = '#606B70', bg = '#1E2227' },
+            -- close_button_visible  = { fg = '#606B70', bg = '#1E2227' },
+            -- close_button_selected = { fg = '#DCDCDC', bg = '#23272E' },
 
-                buffer_visible        = { fg = '#606B70', bg = '#1E2227' },
-                buffer_selected       = { fg = '#DCDCDC', bg = '#23272E', bold = true, italic = false },
 
-                numbers_visible       = { fg = '#606B70', bg = '#1E2227' },
-                numbers_selected      = { fg = '#DCDCDC', bg = '#23272E', bold = true, italic = false },
-                -- 
-                --
-                modified              = { fg = '#E2B86B'},
-                modified_visible      = { fg = '#E2B86B', bg = '#1E2227' },
-                modified_selected     = { fg = '#E2B86B', bg = '#23272E' },
-                --
-                --            separator             = { bg = '#23272E' },
-                --            separator_visible     = { fg = '#606B70', bg = '#1E2227' },
-                --            separator_selected    = { fg = '#DCDCDC', bg = '#23272E' },
 
-                -- indicator_visible     = { fg = '#61AFEF', bg = '#1E2227' },
-                indicator_selected    = { fg = '#61AFEF', bg = '#23272E' },
+            buffer_visible        = { fg = '#ABB2BF', bg = '#21252B' },
+            buffer_selected       = { fg = '#DCDCDC', bg = '#282C34', bold = true, italic = false },
 
-            },
+            -- numbers_visible       = { fg = '#606B70', bg = '#1E2227' },
+            -- numbers_selected      = { fg = '#DCDCDC', bg = '#23272E', bold = true, italic = false },
+
+            modified              = { fg = '#E2B86B'},
+            modified_visible      = { fg = '#E2B86B', bg = '#21252B' },
+            modified_selected     = { fg = '#E2B86B', bg = '#282C34' },
+
+            separator             = { fg = '#181A1F' },
+            separator_visible     = { fg = '#181A1F', bg = '#21252B' },
+            separator_selected    = { fg = '#181A1F', bg = '#282C34' },
+
+            -- indicator_visible     = { fg = '#61AFEF', bg = '#1E2227' },
+            indicator_selected    = { fg = '#61AFEF', bg = '#23272E', sp = "#61afef" },
+
         },
     },
 
--- nvim/lua/plugins/neotree.lua
+++ nvim/lua/plugins/neotree.lua
@@ -11,215 +11,260 @@ return {
     },
     keys = {
         { '<leader>|', ':Neotree reveal filesystem float toggle=true<cr>', desc = 'Toggle file tree (float)' },
-        { '<leader>\\', ':Neotree reveal filesystem show left toggle focus<cr>', desc = 'Toggle file tree (sidebar)' },
         { '<localleader>e', ':Neotree toggle<cr>', desc = 'Toggle file tree (sidebar)' },
     },
-  config = function()
+    config = function()
 
-    local tree = require 'neo-tree'
-    local highlights = require 'neo-tree.ui.highlights'
-    local renderer = require 'neo-tree.ui.renderer'
+        local tree = require 'neo-tree'
+        local highlights = require 'neo-tree.ui.highlights'
+        local renderer = require 'neo-tree.ui.renderer'
 
-    -- ascend to the parent or close it
-    local function float(state)
-      local node = state.tree:get_node()
-      if (node.type == 'directory' or node:has_children()) and node:is_expanded() then
-        state.commands.toggle_node(state)
-      else
-        renderer.focus_node(state, node:get_parent_id())
-      end
-    end
-
-    -- toggle a node open or descend to it's first child
-    local function dive(state)
-      local node = state.tree:get_node()
-      if node.type == 'directory' or node:has_children() then
-        if not node:is_expanded() then
-          if node.type == 'directory' then
-            require 'neo-tree.sources.filesystem'.toggle_directory(state, node)
-          else
-            state.commands.toggle_node(state)
-          end
-        else
-          renderer.focus_node(state, node:get_child_ids()[1])
+        -- ascend to the parent or close it
+        local function float(state)
+            local node = state.tree:get_node()
+            if (node.type == 'directory' or node:has_children()) and node:is_expanded() then
+                state.commands.toggle_node(state)
+            else
+                renderer.focus_node(state, node:get_parent_id())
+            end
         end
-      end
-    end
 
-    tree.setup {
-      close_if_last_window = false, -- Close Neo-tree if it is the last window left in the tab
-      -- popup_border_style = "rounded",
-      popup_border_style = "single",
-      enable_git_status = true,
-      enable_diagnostics = true,
-      use_libuv_filewatcher = true,
+        -- toggle a node open or descend to it's first child
+        local function dive(state)
+            local node = state.tree:get_node()
+            if node.type == 'directory' or node:has_children() then
+                if not node:is_expanded() then
+                    if node.type == 'directory' then
+                        require 'neo-tree.sources.filesystem'.toggle_directory(state, node)
+                    else
+                        state.commands.toggle_node(state)
+                    end
+                else
+                    renderer.focus_node(state, node:get_child_ids()[1])
+                end
+            end
+        end
 
-      source_selector = {
-        winbar = true
-      },
+        tree.setup {
+            add_blank_line_at_top = true,                                      -- 在顶部添加空行
+            close_if_last_window = true,                                       -- 如果Neo树是选项卡中剩下的最后一个窗口，请关闭它
+            close_floats_on_escape_key = true,
+            popup_border_style = "NC",                                          -- "double", "none", "rounded", "shadow", "single" or "solid"
+            enable_git_status = true,
+            enable_diagnostics = false,
+            use_libuv_filewatcher = true,
+            git_status_async = true,
+            git_status_async_options = {
+                batch_size = 1000,
+                batch_delay = 10,
+                max_lines = 10000,
+  
+            },
+            hide_root_node = false,                                            -- 隐藏根节点
+            retain_hidden_root_indent = false,                                 -- 如果根节点被隐藏，则无论如何都要保留缩进
+            resize_timer_interval = 500,
+            sort_ase_insensitive=false,                                        --在对树中的文件和目录进行排序时使用
+            sort_function=nil,                                                 --使用自定义函数对树中的文件和目录进行排序
 
+            source_selector = {
+                winbar = false, -- toggle to show selector on winbar
+                statusline = false, -- toggle to show selector on statusline
+                show_scrolled_off_parent_node = false, -- this will replace the tabs with the parent path
+                                                    -- of the top visible node when scrolled down.
+                tab_labels = { -- falls back to source_name if nil
+                    filesystem = "  Files ",
+                    buffers =    "  Buffers ",
+                    git_status = "  Git ",
+                    diagnostics = " 裂Diagnostics ",
+                },
+                content_layout = "start", -- only with `tabs_layout` = "equal", "focus"
+                tabs_layout = "equal", -- start, end, center, equal, focus
+                truncation_character = "…", -- character to use when truncating the tab label
+                tabs_min_width = nil, -- nil | int: if int padding is added based on `content_layout`
+                tabs_max_width = nil, -- this will truncate text even if `text_trunc_to_fit = false`
+                padding = 0, -- can be int or table
+                separator = { left = "▏", right= "▕" },
+                separator_active = nil, -- set separators around the active tab. nil falls back to `source_selector.separator`
+                show_separator_on_edge = false,
+                highlight_tab = "NeoTreeTabInactive",
+                highlight_tab_active = "NeoTreeTabActive",
+                highlight_background = "NeoTreeTabInactive",
+                highlight_separator = "NeoTreeTabSeparatorInactive",
+                highlight_separator_active = "NeoTreeTabSeparatorActive",
+            },
 
-      default_component_configs = {
-        indent = {
-          indent_size = 2,
-          padding = 1, -- extra padding on left hand side
-          -- indent guides
-          with_markers = true,
-          indent_marker = "│",
-          last_indent_marker = "└",
-          highlight = "NeoTreeIndentMarker",
-          -- expander config, needed for nesting files
-          with_expanders = nil, -- if nil and file nesting is enabled, will enable expanders
-          expander_collapsed = "",
-          expander_expanded = "",
-          expander_highlight = "NeoTreeExpander",
+            default_component_configs = {
+                container = {
+                    enable_character_fade = true,
+                    width = "100%",
+                    right_padding = 0,
+                },
+                indent = {
+                    indent_size = 2,
+                    padding = 1, -- extra padding on left hand side
+                    -- indent guides
+                    with_markers = true,
+                    indent_marker = "│",
+                    last_indent_marker = "└",
+                    highlight = "NeoTreeIndentMarker",
+                    -- expander config, needed for nesting files
+                    with_expanders = nil, -- if nil and file nesting is enabled, will enable expanders
+                    expander_collapsed = "",
+                    expander_expanded = "",
+                    expander_highlight = "NeoTreeExpander",
+                },
+
+                icon = {
+                    -- folder_closed = "",
+                    -- folder_open = "",
+                    folder_closed = "",
+                    folder_open = "",
+                    folder_empty = "ﰊ",
+                    default = "·",
+                    highlight = "NeoTreeFileIcon"
+                },
+                modified = {
+                    symbol = "[+] ",
+                    highlight = "NeoTreeModified",
+                    },
+                name = {
+                    trailing_slash = false,
+                    use_git_status_colors = true,
+                    highlight = "NeoTreeFileName",
+                },
+
+                git_status = {
+                    symbols = {
+                        -- Change type
+                        added     = "✚",
+                        deleted   = "✖",
+                        modified  = "",
+                        renamed   = "",
+                        -- Status type
+                        untracked = "",
+                        ignored   = "",
+                        unstaged  = "",
+                        staged    = "",
+                        conflict  = "",
+                    },
+                    align = "right",
+                },
+            },
+
+            window = {
+                position = "left",
+                width = 40,
+                mappings = {
+                    ["<2-LeftMouse>"] = "open",
+                    ["<cr>"] = "open",
+                    ["S"] = "open_split",
+                    ["s"] = "open_vsplit",
+                    ["C"] = "close_node",
+                    ["<bs>"] = "navigate_up",
+                    ["."] = "set_root",
+                    ["H"] = "toggle_hidden",
+                    ["R"] = "refresh",
+                    ["/"] = "fuzzy_finder",
+                    ["f"] = "filter_on_submit",
+                    ["<c-x>"] = "clear_filter",
+                    ["a"] = "add",
+                    ["d"] = "delete",
+                    ["r"] = "rename",
+                    ["y"] = "copy_to_clipboard",
+                    ["x"] = "cut_to_clipboard",
+                    ["p"] = "paste_from_clipboard",
+                    ["c"] = "copy", -- takes text input for destination
+                    ["m"] = "move", -- takes text input for destination
+                    ["q"] = "close_window",
+                    ["h"] = float,
+                    ["l"] = dive,
+                    ["<Left>"] = float,
+                    ["<Right>"] = dive,
+                }
+            },
+
+            nesting_rules = {
+                ts = { 'js', 'js.map', 'd.ts' },
+                scss = { 'css', 'min.css', 'css.map', 'min.css.map' },
+            },
+
+            filesystem = {
+                follow_current_file = true, -- This will find and focus the file in the active buffer every time the current file is changed while the tree is open.
+                hijack_netrw_behavior = "open_default", -- netrw disabled, opening a directory opens neo-tree in whatever position is specified in window.position
+                filtered_items = {
+                    visible = true, -- when true, they will just be displayed differently than normal items
+                    hide_dotfiles = true,
+                    hide_gitignored = true,
+                    hide_by_name = {
+                        ".DS_Store",
+                        "thumbs.db"
+                        --"node_modules"
+                    },
+                    never_show = { -- remains hidden even if visible is toggled to true
+                    --".DS_Store",
+                    --"thumbs.db"
+                },
+            },
+
+            components = {
+
+                icon = function(config, node)
+                    local icon = config.default or ' '
+                    local padding = config.padding or ' '
+                    local highlight = config.highlight or highlights.FILE_ICON
+                    local web_devicons = require 'nvim-web-devicons'
+                    if node.type == 'directory' then
+                        highlight = highlights.DIRECTORY_ICON
+                        if node.name == 'node_modules' or node.name == '.git' or node.name == '.github' then
+                            local _icon, _highlight = web_devicons.get_icon(node.name)
+                            icon = _icon or icon
+                            highlight = _highlight or highlight
+                        elseif node:is_expanded() then
+                            icon = config.folder_open or '-'
+                        else
+                            icon = config.folder_closed or '+'
+                        end
+                    elseif node.type == 'file' then
+                        ---@type string
+                        local name = node.name
+                        local ext = node.ext
+                        if ext == 'json' and name:match[[^tsconfig]] then name = 'tsconfig.json' end
+                        local _icon, _highlight = web_devicons.get_icon(name, ext)
+                        icon = _icon or icon
+                        highlight = _highlight or highlight
+                    end
+                    return {
+                        text = icon .. padding,
+                        highlight = highlight,
+                    }
+                end,
+
+            },
         },
 
-        icon = {
-          -- folder_closed = "",
-          -- folder_open = "",
-          folder_closed = "",
-          folder_open = "",
-          folder_empty = "ﰊ",
-          default = "*",
-        },
-
-        name = {
-          trailing_slash = false,
-          use_git_status_colors = true,
+        buffers = {
+            show_unloaded = true,
+            window = {
+                mappings = {
+                    ["bd"] = "buffer_delete",
+                }
+            },
         },
 
         git_status = {
-          symbols = {
-            -- Change type
-            added     = "✚",
-            deleted   = "✖",
-            modified  = "",
-            renamed   = "",
-            -- Status type
-            untracked = "",
-            ignored   = "",
-            unstaged  = "",
-            staged    = "",
-            conflict  = "",
-          }
-        },
-      },
-
-      window = {
-        position = "left",
-        width = 40,
-        mappings = {
-          ["<2-LeftMouse>"] = "open",
-          ["<cr>"] = "open",
-          ["S"] = "open_split",
-          ["s"] = "open_vsplit",
-          ["C"] = "close_node",
-          ["<bs>"] = "navigate_up",
-          ["."] = "set_root",
-          ["H"] = "toggle_hidden",
-          ["R"] = "refresh",
-          ["/"] = "fuzzy_finder",
-          ["f"] = "filter_on_submit",
-          ["<c-x>"] = "clear_filter",
-          ["a"] = "add",
-          ["d"] = "delete",
-          ["r"] = "rename",
-          ["y"] = "copy_to_clipboard",
-          ["x"] = "cut_to_clipboard",
-          ["p"] = "paste_from_clipboard",
-          ["c"] = "copy", -- takes text input for destination
-          ["m"] = "move", -- takes text input for destination
-          ["q"] = "close_window",
-          ["h"] = float,
-          ["l"] = dive,
-          ["<Left>"] = float,
-          ["<Right>"] = dive,
-        }
-      },
-
-      nesting_rules = {
-        ts = { 'js', 'js.map', 'd.ts' },
-        scss = { 'css', 'min.css', 'css.map', 'min.css.map' },
-      },
-
-      filesystem = {
-        follow_current_file = true, -- This will find and focus the file in the active buffer every time the current file is changed while the tree is open.
-        hijack_netrw_behavior = "open_default", -- netrw disabled, opening a directory opens neo-tree in whatever position is specified in window.position
-        filtered_items = {
-          visible = true, -- when true, they will just be displayed differently than normal items
-          hide_dotfiles = true,
-          hide_gitignored = true,
-          hide_by_name = {
-            ".DS_Store",
-            "thumbs.db"
-            --"node_modules"
-          },
-          never_show = { -- remains hidden even if visible is toggled to true
-            --".DS_Store",
-            --"thumbs.db"
-          },
-        },
-
-        components = {
-
-          icon = function(config, node)
-            local icon = config.default or ' '
-            local padding = config.padding or ' '
-            local highlight = config.highlight or highlights.FILE_ICON
-            local web_devicons = require 'nvim-web-devicons'
-            if node.type == 'directory' then
-              highlight = highlights.DIRECTORY_ICON
-              if node.name == 'node_modules' or node.name == '.git' or node.name == '.github' then
-                local _icon, _highlight = web_devicons.get_icon(node.name)
-                icon = _icon or icon
-                highlight = _highlight or highlight
-              elseif node:is_expanded() then
-                icon = config.folder_open or '-'
-              else
-                icon = config.folder_closed or '+'
-              end
-            elseif node.type == 'file' then
-              ---@type string
-              local name = node.name
-              local ext = node.ext
-              if ext == 'json' and name:match[[^tsconfig]] then name = 'tsconfig.json' end
-              local _icon, _highlight = web_devicons.get_icon(name, ext)
-              icon = _icon or icon
-              highlight = _highlight or highlight
-            end
-            return {
-              text = icon .. padding,
-              highlight = highlight,
+            window = {
+                position = "float",
+                mappings = {
+                    ["A"]  = "git_add_all",
+                    ["gu"] = "git_unstage_file",
+                    ["ga"] = "git_add_file",
+                    ["gr"] = "git_revert_file",
+                    ["gc"] = "git_commit",
+                    ["gp"] = "git_push",
+                    ["gg"] = "git_commit_and_push",
+                }
             }
-          end,
-
-        },
-      },
-
-      buffers = {
-        show_unloaded = true,
-        window = {
-          mappings = {
-            ["bd"] = "buffer_delete",
-          }
-        },
-      },
-
-      git_status = {
-        window = {
-          position = "float",
-          mappings = {
-            ["A"]  = "git_add_all",
-            ["gu"] = "git_unstage_file",
-            ["ga"] = "git_add_file",
-            ["gr"] = "git_revert_file",
-            ["gc"] = "git_commit",
-            ["gp"] = "git_push",
-            ["gg"] = "git_commit_and_push",
-          }
         }
-      }
     }
-  end
+end
 }
