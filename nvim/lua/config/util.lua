79d71cfb91c103676b0ad57597a9c769486d0f77
hantaotaohan <hantaotaohan@gmail.com>
2023-02-28 16:39:57 +0800

    fix nvim

---
+++ nvim/lua/config/util.lua
@@ -0,0 +1,78 @@
+local Util = require("lazy.core.util")
+
+local M = {}
+
+---@param on_attach fun(client, buffer)
+local icons = {
+    diagnostics = {
+        Error = " ",
+        Warn = " ",
+        Hint = " ",
+        Info = " ",
+    },
+    git = {
+        added = " ",
+        modified = " ",
+        removed = " ",
+    },
+    kinds = {
+        Array = " ",
+        Boolean = " ",
+        Class = " ",
+        Color = " ",
+        Constant = " ",
+        Constructor = " ",
+        Copilot = " ",
+        Enum = " ",
+        EnumMember = " ",
+        Event = " ",
+        Field = " ",
+        File = " ",
+        Folder = " ",
+        Function = " ",
+        Interface = " ",
+        Key = " ",
+        Keyword = " ",
+        Method = " ",
+        Module = " ",
+        Namespace = " ",
+        Null = "ﳠ ",
+        Number = " ",
+        Object = " ",
+        Operator = " ",
+        Package = " ",
+        Property = " ",
+        Reference = " ",
+        Snippet = " ",
+        String = " ",
+        Struct = " ",
+        Text = " ",
+        TypeParameter = " ",
+        Unit = " ",
+        Value = " ",
+        Variable = " ",
+    },
+},
+
+---@param on_attach fun(client, buffer)
+function M.on_attach(on_attach)
+    vim.api.nvim_create_autocmd("LspAttach", {
+        callback = function(args)
+            local buffer = args.buf
+            local client = vim.lsp.get_client_by_id(args.data.client_id)
+            on_attach(client, buffer)
+        end,
+    })
+end
+
+---@param plugin string
+function M.has(plugin)
+    return require("lazy.core.config").plugins[plugin] ~= nil
+end
+
+---@param plugin string
+function M.has(plugin)
+  return require("lazy.core.config").plugins[plugin] ~= nil
+end
+
+return M
