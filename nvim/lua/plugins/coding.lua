23f1188e345e37d615d92f78344109af1cc23cf2
hantaotaohan <hantaotaohan@gmail.com>
2023-03-30 10:03:40 +0800

    fix box

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -37,52 +37,201 @@ return {
 		config = function()
 			local ls = require("luasnip")
 			local s = ls.snippet
-			local t = ls.text_node
 			local i = ls.insert_node
 			local f = ls.function_node
+			local fmt = require("luasnip.extras.fmt").fmt
 
-			local function box(opts)
-				local function box_width()
-					return opts.box_width or vim.opt.textwidth:get()
-				end
-
-				local function padding(cs, input_text)
-					local spaces = box_width() - (2 * #cs)
-					spaces = spaces - #input_text
-					return spaces / 2
-				end
-
-				local comment_string = function()
-					return require("luasnip.util.util").buffer_comment_chars()[1]
-				end
-
-				return {
-					f(function()
-						local cs = comment_string()
-						return string.rep(string.sub(cs, 1, 1), box_width())
-					end, { 1 }),
-					t({ "", "" }),
-					f(function(args)
-						local cs = comment_string()
-						return cs .. string.rep(" ", math.floor(padding(cs, args[1][1])))
-					end, { 1 }),
-					i(1, "placeholder"),
-					f(function(args)
-						local cs = comment_string()
-						return string.rep(" ", math.ceil(padding(cs, args[1][1]))) .. cs
-					end, { 1 }),
-					t({ "", "" }),
-					f(function()
-						local cs = comment_string()
-						return string.rep(string.sub(cs, 1, 1), box_width())
-					end, { 1 }),
-				}
+			local replace = function(index, char)
+				return f(function(arg)
+					return string.rep(char, string.len(arg[1][1]))
+				end, { index })
 			end
+
+			local parse_comments = function()
+				local iter = vim.gsplit(vim.o.comments, ",", true)
+				local comments = { single = {}, triple = {}, other = {} }
+
+				local split = function(cs)
+					local flags, text = string.match(cs or "", "^([^:]*):([^:]*)$")
+					return flags or "", text or ""
+				end
+
+				while true do
+					local cs = iter()
+					if not cs then
+						break
+					end
+					local flags, text = split(cs)
+
+					if #flags == 0 then
+						-- Parse the single line comment string.
+						table.insert(comments.other, 1, { start = text, mid = text, stop = text, indent = "" })
+					elseif flags:match("s") and not flags:match("O") then
+						-- Parse 3 part comment string, but ignore those with the O flag
+						local ctriple = {}
+						local indent = ""
+
+						if string.match(flags:sub(-1), "%d+") then
+							indent = string.rep(" ", flags:sub(-1))
+						end
+						ctriple.start = text
+
+						cs = iter()
+						flags, text = split(cs)
+						assert(vim.startswith(flags, "m"), "Expected middle comment format of triple")
+						ctriple.mid = text
+
+						cs = iter()
+						flags, text = split(cs)
+						assert(vim.startswith(flags, "e"), "Expected end comment format of triple")
+						ctriple.stop = text
+						ctriple.indent = indent
+
+						table.insert(comments.triple, 1, ctriple)
+					elseif flags:match("b") then
+						if #text == 1 then
+							table.insert(comments.single, 1, { start = text, mid = text, stop = text, indent = "" })
+						end
+					end
+				end
+
+				return comments
+			end
+
+			local comment_format = function()
+				if vim.o.commentstring:match("%%s$") then
+					-- Remove last two characters
+					local cf
+					cf = vim.trim(vim.o.commentstring:sub(1, -3))
+					return { start = cf, mid = cf, stop = cf, indent = "" }
+				end
+
+				local comments = parse_comments()
+				if not vim.tbl_isempty(comments.single) then
+					return comments.single[1]
+				end
+
+				if not vim.tbl_isempty(comments.other) then
+					return comments.other[1]
+				end
+
+				return comments.triple[1]
+			end
+
+			local comment = function(part)
+				return f(function()
+					if vim.o.filetype == "" then
+						return ""
+					end
+
+					local cf = comment_format()
+					local c
+					if part == "mid" then
+						c = cf.indent .. cf[part]
+					else
+						c = cf[part]
+					end
+
+					if c ~= "" then
+						c = c .. " "
+					end
+
+					return c
+				end, {})
+			end
+
+			local fill = function(char, padding, part)
+				return f(function()
+					local clen = string.len(comment_format()[part])
+					return string.rep(char, vim.bo.textwidth - padding - clen)
+				end)
+			end
+
+			local center = function(index, char, padding, part, side)
+				return f(function(arg)
+					local cf = comment_format()
+					local clen
+					if part == "mid" then
+						clen = string.len(cf.indent .. cf[part])
+					else
+						clen = string.len(cf[part])
+					end
+					local text = string.len(arg[1][1])
+					local count = vim.bo.textwidth - padding - text - clen
+					if count % 2 == 1 and side == "left" then
+						count = (count + 1) / 2
+					else
+						count = count / 2
+					end
+					return string.rep(char, count)
+				end, { index })
+			end
+
+			-- ------------------------------------------------------------------------
+			-- | Wide Boxes
+			-- ------------------------------------------------------------------------
 
 			require("luasnip").add_snippets("all", {
 				-- https://github.com/L3MON4D3/LuaSnip/wiki/Cool-Snippets#box-comment-like-ultisnips
-				s({ trig = "box" }, box({ box_width = 24 })),
-				s({ trig = "bbox" }, box({ box_width = 80 })),
+				s(
+					"box",
+					fmt(
+						[[
+                            {comment_start}┌───{filler}───┐
+                            {comment_mid}│   {left_center}{insert}{right_center}   │
+                            {comment_stop}└───{filler}───┘
+                        ]],
+						{
+							comment_start = comment("start"),
+							comment_mid = comment("mid"),
+							comment_stop = comment("stop"),
+							filler = fill("─", 9, "start"),
+							left_center = center(1, " ", 9, "mid", "left"),
+							insert = i(1),
+							right_center = center(1, " ", 9, "mid", "right"),
+						}
+					)
+				),
+				s(
+					"bbox",
+					fmt(
+						[[
+                            {comment_start}┌───{filler}───┐
+                            {comment_mid}│   {space_filler}   │
+                            {comment_mid}│   {left_center}{insert}{right_center}   │
+                            {comment_mid}│   {space_filler}   │
+                            {comment_stop}└───{filler}───┘
+                        ]],
+						{
+							comment_start = comment("start"),
+							comment_mid = comment("mid"),
+							comment_stop = comment("stop"),
+							filler = fill("─", 9, "start"),
+							space_filler = fill(" ", 9, "start"),
+							left_center = center(1, " ", 9, "mid", "left"),
+							insert = i(1),
+							right_center = center(1, " ", 9, "mid", "right"),
+						}
+					)
+				),
+
+				s(
+					"sbox",
+					fmt(
+						[[
+                            {3}┌───{2}───┐
+                            {4}│   {1}   │
+                            {5}└───{2}───┘
+                        ]],
+						{
+							i(1),
+							replace(1, "─"),
+							comment("start"),
+							comment("mid"),
+							comment("stop"),
+						}
+					)
+				),
 			})
 		end,
 		opts = {
